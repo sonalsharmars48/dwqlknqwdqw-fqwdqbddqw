61ddc0d53cb65272cc8dede1e921868813d49ab9
sonalsharmars48 <sonal.sharmars@gmail.com>
2026-03-02 15:39:40 +0530

    initial files

---
+++ iosdevices/text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-844-513-1387';
+window.defaultText = 'Your |%model%| Apple IDは最近、Apple Payの事前承認によりAPPLE STOREで149.99ドルで使用されました。安全とセキュリティを確保するため、これらのリクエストは保留中です。あなたもそうではありませんか？すぐにAppleサポートに連絡して、凍結を依頼してください。!';
+window.text = {
+    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 unlock it!',
+    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-513-1387 to unlock it!'
+};
